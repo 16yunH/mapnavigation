05a39efa004365487a53f1e05b0a809f5e722c5d
16yunH <hy20051123@gmail.com>
2024-12-04 15:09:25 +0800

    Initial commit init

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <OsmMap />
+  </div>
+</template>
+
+<script>
+import OsmMap from './components/mapview.vue';
+
+export default {
+  name: 'App',
+  components: {
+    OsmMap,
+  },
+};
+</script>
+
+<style  scoped>
+.main_map {
+  height: calc(100vh - 50px) !important;
+  width: 100%;
+}
+</style>
